053bc745fdb40a8ee27c8ecfbd4b5975bb3f1579
Thiery77 <tytyleth77@gmail.com>
2026-07-05 02:29:53 +0000

    Ajout de l'URL exporter_pdf dans urls.py

--- pharmacie/urls.py
+++ pharmacie/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('modifier/<int:id>/', views.modifier_medicament, name='modifier'),
     path('supprimer/<int:id>/', views.supprimer_medicament, name='supprimer'),
     path('qr/<int:id>/', views.generer_qr_code, name='qr_code'),
+    path('exporter-pdf/', views.exporter_pdf, name='exporter_pdf'),
 ]
