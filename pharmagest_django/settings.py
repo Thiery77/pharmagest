8b4c3b20edc539eb4a4bbc8427da1ff7650491b4
Thiery77 <tytyleth77@gmail.com>
2026-07-05 05:30:16 +0000

    Redirection vers le tableau de bord après connexion

--- pharmagest_django/settings.py
+++ pharmagest_django/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/tableau/'
 LOGIN_URL = '/accounts/login/'
 
 # Configuration Django-Q
